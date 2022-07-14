f417d6b965a3198e4a42ee8c55f38adadaddc1c9
Ghaasen Hakiri <ghassen25hakiri@gmail.com>
2022-07-14 17:20:14 +0200

    name

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,9 +11,9 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "MyTeam";
-        } else { // TODO extend the programm here
+        } else if (query.toLowerCase().contains("name")) {
+           return "ghass";
+        } else {
             return "";
         }
     }
